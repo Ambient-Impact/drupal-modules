fddba5ad2a586b32c3ea75cf7112375146991632
Ambient.Impact <i@ambientimpact.com>
2019-03-12 11:21:34 -0400

    Ported pointer_focus_hide component.

---
+++ ambientimpact_core/src/Plugin/AmbientImpact/Component/PointerFocusHide.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_core\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Hide pointer focus component.
+ *
+ * @Component(
+ *   id = "pointer_focus_hide",
+ *   title = @Translation("Hide pointer focus"),
+ *   description = @Translation("This component can hide the focus outline if triggered by a pointer.")
+ * )
+ */
+class PointerFocusHide extends ComponentBase {
+}
