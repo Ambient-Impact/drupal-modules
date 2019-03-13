063c7c1cfa3ebc28042cbd21e6e6174f5ae389ca
Ambient.Impact <i@ambientimpact.com>
2019-03-13 11:49:37 -0400

    Added missing Environment component plugin. D:

---
+++ ambientimpact_core/src/Plugin/AmbientImpact/Component/Environment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_core\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Environment component.
+ *
+ * @Component(
+ *   id = "environment",
+ *   title = @Translation("Environment"),
+ *   description = @Translation("A simple framework for feature and environment testing.")
+ * )
+ */
+class Environment extends ComponentBase {
+}
