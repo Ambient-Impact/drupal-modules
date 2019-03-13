20a03f570d6098f6f33ae51367cc6ffc5df5035a
Ambient.Impact <i@ambientimpact.com>
2019-03-13 09:16:45 -0400

    Added currently unused contextual component plugin.

---
+++ ambientimpact_core/src/Plugin/AmbientImpact/Component/Contextual.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_core\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Contextual Links component.
+ *
+ * @Component(
+ *   id = "contextual",
+ *   title = @Translation("Contextual Links"),
+ *   description = @Translation("This component contains modifications and enhancements to the Drupal contextual links.")
+ * )
+ */
+class Contextual extends ComponentBase {
+}
