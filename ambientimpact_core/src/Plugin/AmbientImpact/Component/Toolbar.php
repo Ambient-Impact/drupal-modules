1c8936cebe4b6e137093f719d7b52541096b7c4b
Ambient.Impact <i@ambientimpact.com>
2019-03-13 09:16:24 -0400

    Added a currently unused toolbar component.

---
+++ ambientimpact_core/src/Plugin/AmbientImpact/Component/Toolbar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_core\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Toolbar component.
+ *
+ * @Component(
+ *   id = "toolbar",
+ *   title = @Translation("Toolbar"),
+ *   description = @Translation("This component contains modifications and enhancements to the Drupal administrative toolbar.")
+ * )
+ */
+class Toolbar extends ComponentBase {
+}
