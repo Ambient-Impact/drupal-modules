e67e237d9ff5a69ed57b151ad2c7dfbd379e1ed4
Ambient.Impact <i@ambientimpact.com>
2019-03-18 12:57:00 -0400

    Removed Grunt uglify and minified JS; now handled by AdvAgg module.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,13 +24,11 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-modernizr');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-svgstore');
 
 	grunt.registerTask('all', [
 		'sass',
 		'autoprefixer',
-		'uglify',
 		'svgstore',
 	]);
 
