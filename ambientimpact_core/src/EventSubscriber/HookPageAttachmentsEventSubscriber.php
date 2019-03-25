cb6b80fb26366b85b620d0dddfe86f50f284dfc5
Ambient.Impact <i@ambientimpact.com>
2019-03-25 18:02:17 -0400

    Converted hook_page_attachments() implementation to an event class.

---
+++ ambientimpact_core/src/EventSubscriber/HookPageAttachmentsEventSubscriber.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\ambientimpact_core\EventSubscriber;
+
+use Drupal\ambientimpact_core\ContainerAwareEventSubscriber;
+
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\hook_event_dispatcher\Event\Page\PageAttachmentsEvent;
+
+/**
+ * hook_page_attachments() event subscriber class.
+ */
+class HookPageAttachmentsEventSubscriber
+extends ContainerAwareEventSubscriber {
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      HookEventDispatcherInterface::PAGE_ATTACHMENTS => 'pageAttachments',
+    ];
+  }
+
+  /**
+   * Attach assets to the page element itself.
+   *
+   * This adds the following:
+   *
+   * - 'ambientimpact_core/core' library, which contains common and layout CSS.
+   *
+   * - Framework JavaScript settings to drupalSettings.
+   *
+   * - Component JavaScript settings to drupalSettings; these need to be here
+   *   rather than on individual elements the actual component libraries are
+   *   usually attached to because they may use shared global settings and/or
+   *   counters.
+   *
+   * @param \Drupal\hook_event_dispatcher\Event\Page\PageAttachmentsEvent $event
+   *   The event object.
+   */
+  public function pageAttachments(PageAttachmentsEvent $event) {
+    $attached = &$event->getAttachments()['#attached'];
+
+    $attached['library'][] = 'ambientimpact_core/core';
+
+    $attached['drupalSettings']['AmbientImpact'] = [
+      'framework' => [
+        'cache'   => [
+          'assetKey'  =>
+            $this->container->get('state')->get('system.css_js_query_string'),
+        ],
+      ],
+      'components'  =>
+        $this->container->get('plugin.manager.ambientimpact_component')
+          ->getComponentJSSettings()
+    ];
+  }
+}
