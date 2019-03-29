a1896739ddbb0775a7e894ca514460554690f156
Ambient.Impact <i@ambientimpact.com>
2019-03-28 21:51:31 -0400

    Added empty Material Ripple component files.

---
+++ ambientimpact_core/src/Plugin/AmbientImpact/Component/MaterialRipple.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_core\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Material Ripple component.
+ *
+ * @Component(
+ *   id = "material.ripple",
+ *   title = @Translation("Material Ripple"),
+ *   description = @Translation("Material Design-style ripple effects on various interactions.")
+ * )
+ */
+class MaterialRipple extends ComponentBase {
+}
