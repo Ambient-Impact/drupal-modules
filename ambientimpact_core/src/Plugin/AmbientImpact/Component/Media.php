9039d489cb054d8dbb12abc7a6a7580a4ac1636f
Ambient.Impact <i@ambientimpact.com>
2019-04-04 21:57:53 -0400

    Added media component with Sass for media thumbnail play icon overlay.

---
+++ ambientimpact_core/src/Plugin/AmbientImpact/Component/Media.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_core\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Media Query component.
+ *
+ * @Component(
+ *   id = "media",
+ *   title = @Translation("Media"),
+ *   description = @Translation("This includes various improvements for the core and contrib media functionality.")
+ * )
+ */
+class Media extends ComponentBase {
+}
