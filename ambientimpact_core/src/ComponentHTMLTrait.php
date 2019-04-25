ed709577ecd5a0b5daf281a4d468594fb1fb880e
Ambient.Impact <i@ambientimpact.com>
2019-04-25 08:36:15 -0400

    Added a method for Components to respond whether they have HTML.

--- ambientimpact_core/src/ComponentHTMLTrait.php
+++ ambientimpact_core/src/ComponentHTMLTrait.php
@@ -7,13 +7,12 @@ namespace Drupal\ambientimpact_core;
  */
 trait ComponentHTMLTrait {
   /**
-   * Get any HTML this Component may have available for the front-end.
+   * Get the files system path to this Component's HTML file.
    *
-   * @return string|bool
-   *   If the component has a <component name>.html.twig file in its directory,
-   *   it will be rendered and returned, otherwise false is returned.
+   * @return string
+   *   The Component's <component name>.html.twig file path.
    */
-  public function getHTML() {
+  protected function getHTMLPath() {
     // Get the path to the module implementing this component plugin.
     $modulePath   = $this->container->get('module_handler')
       ->getModule($this->pluginDefinition['provider'])->getPath();
@@ -24,19 +23,37 @@ trait ComponentHTMLTrait {
 
     // This is the full file system path to the file, including the file name
     // and extension.
-    $filePath =
-      DRUPAL_ROOT . '/' . $componentPath . '/' .
+    return DRUPAL_ROOT . '/' . $componentPath . '/' .
       $this->pluginDefinition['id'] . '.html.twig';
+  }
 
+  /**
+   * Determine if this Component has HTML available.
+   *
+   * @return boolean
+   *   True if the <component name>.html.twig file exists, false otherwise.
+   */
+  public function hasHTML() {
+    return file_exists($this->getHTMLPath());
+  }
+
+  /**
+   * Get any HTML this Component may have available for the front-end.
+   *
+   * @return string|bool
+   *   If the component has a <component name>.html.twig file in its directory,
+   *   it will be rendered and returned, otherwise false is returned.
+   */
+  public function getHTML() {
     // Don't proceed if the file doesn't exist.
-    if (!file_exists($filePath)) {
+    if (!$this->hasHTML()) {
       return false;
     }
 
     // Build a render array containing the file contents as an inline template.
     $renderArray = [
       '#type'     => 'inline_template',
-      '#template' => file_get_contents($filePath),
+      '#template' => file_get_contents($this->getHTMLPath()),
     ];
 
     // Render the inline template and return it.
