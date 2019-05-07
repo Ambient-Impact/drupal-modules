b6d145afe32c77e0c9b90012f7f6a7c09cf35d65
Ambient.Impact <i@ambientimpact.com>
2019-05-07 16:23:16 -0400

    Converted Gruntfile.js to 2 space indent.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,39 @@
 module.exports = function(grunt) {
-	'use strict';
+  'use strict';
 
-	// Load our Grunt task configs from external files in the 'grunt' directory.
-	var configs = require('load-grunt-configs')(grunt, {
-		config: {
-			src: 'grunt/*'
-		}
-	});
+  // Load our Grunt task configs from external files in the 'grunt' directory.
+  var configs = require('load-grunt-configs')(grunt, {
+    config: {
+      src: 'grunt/*'
+    }
+  });
 
-	// Initialize the Grunt config with the read config plus the package and
-	// paths merged in.
-	grunt.initConfig(Object.assign(configs, {
-		pkg:				grunt.file.readJSON('package.json'),
+  // Initialize the Grunt config with the read config plus the package and paths
+  // merged in.
+  grunt.initConfig(Object.assign(configs, {
+    pkg:              grunt.file.readJSON('package.json'),
 
-		modulePaths:		'ambientimpact_core,' +
-							'ambientimpact_block',
-		stylesheetPaths:	'stylesheets,components',
-		javascriptPaths:	'javascript,components',
+    modulePaths:      'ambientimpact_core,' +
+                      'ambientimpact_block',
+    stylesheetPaths:  'stylesheets,components',
+    javascriptPaths:  'javascript,components',
 
-		librariesPath:		'assets/vendor'
-	}));
+    librariesPath:    'assets/vendor'
+  }));
 
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-modernizr');
-	grunt.loadNpmTasks('grunt-svgstore');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-modernizr');
+  grunt.loadNpmTasks('grunt-svgstore');
 
-	grunt.registerTask('all', [
-		'sass',
-		'autoprefixer',
-		'svgstore',
-	]);
+  grunt.registerTask('all', [
+    'sass',
+    'autoprefixer',
+    'svgstore',
+  ]);
 
-	grunt.registerTask('css', [
-		'sass',
-		'autoprefixer',
-	]);
+  grunt.registerTask('css', [
+    'sass',
+    'autoprefixer',
+  ]);
 };
