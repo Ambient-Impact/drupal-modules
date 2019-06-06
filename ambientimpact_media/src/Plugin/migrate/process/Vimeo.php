5885d6ac5f8c61d8b64f7d67c74326f8f7a29423
Ambient.Impact <i@ambientimpact.com>
2019-06-06 12:23:41 -0400

    Added Vimeo and YouTube Migrate process plug-ins.

---
+++ ambientimpact_media/src/Plugin/migrate/process/Vimeo.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\ambientimpact_media\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Process plugin to convert vimeo:// scheme URIs to video URLs.
+ *
+ * @see https://www.previousnext.com.au/blog/migrating-drupal-7-file-entities-drupal-8-media-entities
+ *   Based on code from this blog post.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "vimeo"
+ * )
+ */
+class Vimeo extends ProcessPluginBase {
+  /**
+   * The file scheme used in the Drupal 7 'file_managed' table.
+   */
+  const SCHEME = 'vimeo://';
+
+  /**
+   * The base URL for Vimeo videos; used to transform SCHEME to full URLs.
+   */
+  const BASE_URL = 'https://vimeo.com/';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform(
+    $value,
+    MigrateExecutableInterface $migrate_executable,
+    Row $row, $destination_property
+  ) {
+    // Convert vimeo:// scheme URI to video URL.
+    if (strpos($value, static::SCHEME) !== false) {
+      $value = static::BASE_URL .
+        explode('/', str_replace(static::SCHEME, '', $value), 2)[1];
+
+    } else {
+      $value = null;
+    }
+
+    return $value;
+  }
+}
---
+++ ambientimpact_media/src/Plugin/migrate/process/YouTube.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\ambientimpact_media\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Process plugin to convert youtube:// scheme URIs to video URLs.
+ *
+ * @see https://www.previousnext.com.au/blog/migrating-drupal-7-file-entities-drupal-8-media-entities
+ *   Based on code from this blog post.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "youtube"
+ * )
+ */
+class YouTube extends ProcessPluginBase {
+  /**
+   * The file scheme used in the Drupal 7 'file_managed' table.
+   */
+  const SCHEME = 'youtube://';
+
+  /**
+   * The base URL for YouTube videos; used to transform SCHEME to full URLs.
+   */
+  const BASE_URL = 'https://youtube.com/watch?';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform(
+    $value,
+    MigrateExecutableInterface $migrate_executable,
+    Row $row, $destination_property
+  ) {
+    // Convert youtube:// scheme URI to video URL.
+    if (strpos($value, static::SCHEME) !== false) {
+      $value = static::BASE_URL . implode(
+        '=',
+        explode('/', str_replace(static::SCHEME, '', $value), 2)
+      );
+
+    } else {
+      $value = null;
+    }
+
+    return $value;
+  }
+}
