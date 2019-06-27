723d7ef1962e316252298cbbc926d788b390a2f6
Ambient.Impact <i@ambientimpact.com>
2019-06-27 12:00:43 -0400

    Added an interface for ComponentPluginManager.

--- ambientimpact_core/src/ComponentPluginManager.php
+++ ambientimpact_core/src/ComponentPluginManager.php
@@ -8,13 +8,15 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Url;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\ambientimpact_core\Annotation\Component as ComponentAnnotation;
+use Drupal\ambientimpact_core\ComponentPluginManagerInterface;
 
 /**
- * Plugin manager for Ambient.Impact Component plugins.
+ * Plug-in manager for Ambient.Impact Component plug-ins.
  */
-class ComponentPluginManager extends DefaultPluginManager {
+class ComponentPluginManager extends DefaultPluginManager
+implements ComponentPluginManagerInterface{
   /**
-   * An associative array of Component instances, keyed by plugin ID.
+   * An associative array of Component instances, keyed by plug-in ID.
    *
    * @var array
    */
@@ -45,7 +47,7 @@ class ComponentPluginManager extends DefaultPluginManager {
    *
    * @param \Traversable $namespaces
    *   An object that implements \Traversable which contains the root paths
-   *   keyed by the corresponding namespace to look for plugin
+   *   keyed by the corresponding namespace to look for plug-in
    *   implementations.
    *
    * @param \Drupal\Core\Cache\CacheBackendInterface $cacheBackend
@@ -64,49 +66,41 @@ class ComponentPluginManager extends DefaultPluginManager {
     ModuleHandlerInterface $moduleHandler
   ) {
     parent::__construct(
-      // This tells the plugin manager to look for Component plugins in
-      // the 'src/Plugin/AmbientImpact/Component' subdirectory of any
-      // enabled modules. This also serves to define the PSR-4
-      // subnamespace in which Component plugins will live.
+      // This tells the plug-in manager to look for Component plug-ins in the
+      // 'src/Plugin/AmbientImpact/Component' subdirectory of any enabled
+      // modules. This also serves to define the PSR-4 subnamespace in which
+      // Component plug-ins will live.
       'Plugin/AmbientImpact/Component',
 
       $namespaces,
 
       $moduleHandler,
 
-      // The name of the interface that plugins should adhere to. Drupal will
-      // enforce this as a requirement. If a plugin does not implement this
+      // The name of the interface that plug-ins should adhere to. Drupal will
+      // enforce this as a requirement. If a plug-in does not implement this
       // interface, Drupal will throw an error.
       ComponentInterface::class,
 
-      // The name of the annotation class that contains the plugin
+      // The name of the annotation class that contains the plug-in
       // definition.
       ComponentAnnotation::class
     );
 
-    // This allows the plugin definitions to be altered by an alter hook.
-    // The parameter defines the name of the hook, thus:
+    // This allows the plug-in definitions to be altered by an alter hook. The
+    // parameter defines the name of the hook, thus:
     // hook_ambientimpact_component_info_alter().
     // @todo Do we even need this?
     $this->alterInfo('ambientimpact_component_info');
 
-    // This sets the caching method for our plugin definitions. Plugin
-    // definitions are discovered by examining the directory defined above,
-    // for any classes with an $plugin_definition_annotation_name. The
-    // annotations are read, and then the resulting data is cached using the
-    // provided cache backend.
+    // This sets the caching method for our plug-in definitions. Plug-in
+    // definitions are discovered by examining the directory defined above, for
+    // any classes with an ComponentAnnotation::class. The annotations are read,
+    // and then the resulting data is cached using the provided cache backend.
     $this->setCacheBackend($cacheBackend, 'ambientimpact_component_info');
   }
 
   /**
-   * Get a Component plugin definition by the plugin ID.
-   *
-   * @param string $componentID
-   *   The plugin ID of the Component.
-   *
-   * @return array|false
-   *   The Component plugin's definition array or false if the Component
-   *   doesn't exist.
+   * {@inheritdoc}
    */
   public function getComponentDefinition(string $componentID) {
     $definitions = $this->getDefinitions();
@@ -114,7 +108,7 @@ class ComponentPluginManager extends DefaultPluginManager {
     // Check if the Component exists so that we can return false without
     // breaking things. If we were to use
     // Drupal\Component\Plugin\PluginManagerBase::getDefinition() here and a
-    // plugin with the ID of $componentID does not exist, we'd get a big ol'
+    // plug-in with the ID of $componentID does not exist, we'd get a big ol'
     // PHP white screen of death.
     if (isset($definitions[$componentID])) {
       return $definitions[$componentID];
@@ -124,21 +118,7 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Get a Component plugin's instance.
-   *
-   * Since each Component should only ever have one instance, this returns any
-   * existing instance that is found, creating one if the $componentID is an
-   * available definition but hasn't been instantiated yet.
-   *
-   * @param string $componentID
-   *   The plugin ID of the Component.
-   *
-   * @return object|false
-   *   The Component plugin instance or false if the $componentID doesn't
-   *   exist in the plugin definitions.
-   *
-   * @see $this->componentInstances
-   *   Component instances are stored here, keyed by $componentID.
+   * {@inheritdoc}
    */
   public function getComponentInstance(string $componentID) {
     // Return an existing instance if found.
@@ -152,7 +132,7 @@ class ComponentPluginManager extends DefaultPluginManager {
       return false;
     }
 
-    // Create the plugin instance.
+    // Create the plug-in instance.
     $this->componentInstances[$componentID] =
       $this->createInstance($componentID, []);
 
@@ -160,16 +140,9 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Get a Component plugin instance's configuration
-   *
-   * @param string $componentID
-   *   The plugin ID of the Component.
-   *
-   * @return array
-   *   The Component's configuration or an empty array if the Component was
-   *   not found.
+   * {@inheritdoc}
    */
-  public function getComponentConfiguration(string $componentID) {
+  public function getComponentConfiguration(string $componentID): array {
     $instance = $this->getComponentInstance($componentID);
 
     if ($instance !== false) {
@@ -180,15 +153,9 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Get all libraries defined by components.
-   *
-   * @return array
-   *   A libraries array in the format hook_library_info_build() expects.
-   *
-   * @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Render!theme.api.php/function/hook_library_info_build
-   * @see https://www.drupal.org/docs/8/creating-custom-modules/adding-stylesheets-css-and-javascript-js-to-a-drupal-8-module
+   * {@inheritdoc}
    */
-  public function getComponentLibraries() {
+  public function getComponentLibraries(): array {
     $libraries = [];
 
     foreach ($this->getDefinitions() as $componentID => $definition) {
@@ -208,16 +175,9 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Get JavaScript settings from all available components.
-   *
-   * @return array
-   *   An array of component settings to pass to drupalSettings, keyed by the
-   *   component plugin ID. Components that return an empty array are ignored.
-   *
-   * @see \Drupal\ambientimpact_core\ComponentJSSettingsInterface::getJSSettings()
-   * @see \Drupal\ambientimpact_core\ComponentJSSettingsTrait::getJSSettings()
+   * {@inheritdoc}
    */
-  public function getComponentJSSettings() {
+  public function getComponentJSSettings(): array {
     $jsSettings = [];
 
     foreach ($this->getDefinitions() as $componentID => $definition) {
@@ -246,23 +206,9 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Get the Component HTML endpoint path.
-   *
-   * This gets the path from Drupal via the route system.
-   *
-   * @return string
-   *   The Component HTML endpoint path.
-   *
-   * @see $this->htmlEndpointPath
-   *   Stores the path so we only have to build it once.
-   *
-   * @see $this->htmlEndpointRoute
-   *   Contains the route name.
-   *
-   * @see \Drupal\ambientimpact_core\EventSubscriber\HookPageAttachmentsEventSubscriber::pageAttachments()
-   *   Passes the path to the front-end.
+   * {@inheritdoc}
    */
-  public function getHTMLEndpointPath() {
+  public function getHTMLEndpointPath(): string {
     if (empty($this->htmlEndpointPath)) {
       $this->htmlEndpointPath =
         Url::fromRoute($this->htmlEndpointRoute)->toString();
@@ -272,17 +218,9 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Get HTML from all available Components.
-   *
-   * @return array
-   *   An array of key/value pairs, where the keys are the lowerCamelCase ID
-   *   of the Component and the value being the rendered HTML of that Component.
-   *   Components that do not provide any HTML are not included in this.
-   *
-   * @see static::camelizeComponentID()
-   *   Converts Component ID to lowerCamelCase.
+   * {@inheritdoc}
    */
-  public function getComponentHTML() {
+  public function getComponentHTML(): array {
     $html = [];
 
     foreach ($this->getDefinitions() as $componentID => $definition) {
@@ -310,13 +248,9 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Get an array of Component IDs that have HTML in lowerCamelCase format.
-   *
-   * @return array
-   *   An array containing zero or more lowerCamelCase Component IDs that have
-   *   HTML available.
+   * {@inheritdoc}
    */
-  public function getComponentNamesWithHTML() {
+  public function getComponentNamesWithHTML(): array {
     $haveHTML = [];
 
     foreach ($this->getDefinitions() as $componentID => $definition) {
@@ -339,29 +273,9 @@ class ComponentPluginManager extends DefaultPluginManager {
   }
 
   /**
-   * Convert a Component ID to lowerCamelCase format.
-   *
-   * This is copied from the Symfony Container::camelize() method, with the
-   * following changes:
-   *
-   * - The first character is lowercased.
-   *
-   * - The only characters used for delimiters are underscores (_), so periods
-   *   (.) are left as-is.
-   *
-   * - Periods (.) are used to denote a sub-component or namespace, so they're
-   *   treated as starting a new lowercase portion.
-   *
-   * @param  string $id
-   *   The Component ID to camelize.
-   *
-   * @return string
-   *   The Component ID in lowerCamelCase.
-   *
-   * @see https://api.drupal.org/api/drupal/vendor%21symfony%21dependency-injection%21Container.php/function/Container%3A%3Acamelize
-   *   The Symfony method that inspired this.
+   * {@inheritdoc}
    */
-  public static function camelizeComponentID(string $id) {
+  public static function camelizeComponentID(string $id): string {
     $camelized  = [];
     $exploded   = explode('.', $id);
 
---
+++ ambientimpact_core/src/ComponentPluginManagerInterface.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Drupal\ambientimpact_core;
+
+/**
+ * Defines an interface for Ambient.Impact Component plug-in managers.
+ */
+interface ComponentPluginManagerInterface {
+  /**
+   * Get a Component plug-in definition by the plug-in ID.
+   *
+   * @param string $componentID
+   *   The plug-in ID of the Component.
+   *
+   * @return array|false
+   *   The Component plug-in's definition array or false if the Component
+   *   doesn't exist.
+   */
+  public function getComponentDefinition(string $componentID);
+
+  /**
+   * Get a Component plug-in's instance.
+   *
+   * Since each Component should only ever have one instance, this returns any
+   * existing instance that is found, creating one if the $componentID is an
+   * available definition but hasn't been instantiated yet.
+   *
+   * @param string $componentID
+   *   The plug-in ID of the Component.
+   *
+   * @return object|false
+   *   The Component plug-in instance or false if the $componentID doesn't
+   *   exist in the plug-in definitions.
+   *
+   * @see $this->componentInstances
+   *   Component instances are stored here, keyed by $componentID.
+   */
+  public function getComponentInstance(string $componentID);
+
+  /**
+   * Get a Component plug-in instance's configuration
+   *
+   * @param string $componentID
+   *   The plug-in ID of the Component.
+   *
+   * @return array
+   *   The Component's configuration or an empty array if the Component was
+   *   not found.
+   */
+  public function getComponentConfiguration(string $componentID): array;
+
+  /**
+   * Get all libraries defined by components.
+   *
+   * @return array
+   *   A libraries array in the format hook_library_info_build() expects.
+   *
+   * @see https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Render!theme.api.php/function/hook_library_info_build
+   * @see https://www.drupal.org/docs/8/creating-custom-modules/adding-stylesheets-css-and-javascript-js-to-a-drupal-8-module
+   */
+  public function getComponentLibraries(): array;
+
+  /**
+   * Get JavaScript settings from all available components.
+   *
+   * @return array
+   *   An array of component settings to pass to drupalSettings, keyed by the
+   *   component plug-in ID. Components that return an empty array are ignored.
+   *
+   * @see \Drupal\ambientimpact_core\ComponentJSSettingsInterface::getJSSettings()
+   * @see \Drupal\ambientimpact_core\ComponentJSSettingsTrait::getJSSettings()
+   */
+  public function getComponentJSSettings(): array;
+
+  /**
+   * Get the Component HTML endpoint path.
+   *
+   * This gets the path from Drupal via the route system.
+   *
+   * @return string
+   *   The Component HTML endpoint path.
+   *
+   * @see $this->htmlEndpointPath
+   *   Stores the path so we only have to build it once.
+   *
+   * @see $this->htmlEndpointRoute
+   *   Contains the route name.
+   *
+   * @see \Drupal\ambientimpact_core\EventSubscriber\HookPageAttachmentsEventSubscriber::pageAttachments()
+   *   Passes the path to the front-end.
+   */
+  public function getHTMLEndpointPath(): string;
+
+  /**
+   * Get HTML from all available Components.
+   *
+   * @return array
+   *   An array of key/value pairs, where the keys are the lowerCamelCase ID
+   *   of the Component and the value being the rendered HTML of that Component.
+   *   Components that do not provide any HTML are not included in this.
+   *
+   * @see static::camelizeComponentID()
+   *   Converts Component ID to lowerCamelCase.
+   */
+  public function getComponentHTML(): array;
+
+  /**
+   * Get an array of Component IDs that have HTML in lowerCamelCase format.
+   *
+   * @return array
+   *   An array containing zero or more lowerCamelCase Component IDs that have
+   *   HTML available.
+   */
+  public function getComponentNamesWithHTML(): array;
+
+  /**
+   * Convert a Component ID to lowerCamelCase format.
+   *
+   * This is copied from the Symfony Container::camelize() method, with the
+   * following changes:
+   *
+   * - The first character is lowercased.
+   *
+   * - The only characters used for delimiters are underscores (_), so periods
+   *   (.) are left as-is.
+   *
+   * - Periods (.) are used to denote a sub-component or namespace, so they're
+   *   treated as starting a new lowercase portion.
+   *
+   * @param  string $id
+   *   The Component ID to camelize.
+   *
+   * @return string
+   *   The Component ID in lowerCamelCase.
+   *
+   * @see https://api.drupal.org/api/drupal/vendor%21symfony%21dependency-injection%21Container.php/function/Container%3A%3Acamelize
+   *   The Symfony method that inspired this.
+   */
+  public static function camelizeComponentID(string $id): string;
+}
