df0f00faf292072763d0afb812eb8bfe265e4e99
Ambient.Impact <i@ambientimpact.com>
2019-06-27 11:15:49 -0400

    Merged Component traits/interfaces into ComponentBase/ComponentInterface.

--- ambientimpact_core/src/ComponentBase.php
+++ ambientimpact_core/src/ComponentBase.php
@@ -5,32 +5,20 @@ namespace Drupal\ambientimpact_core;
 use Drupal\Component\Plugin\PluginBase;
 use Drupal\Component\Plugin\ConfigurableInterface;
 use Drupal\Component\Serialization\SerializationInterface;
+use Drupal\Component\Utility\NestedArray;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Render\RendererInterface;
-use Drupal\ambientimpact_core\ComponentConfigurableTrait;
-use Drupal\ambientimpact_core\ComponentLibrariesInterface;
-use Drupal\ambientimpact_core\ComponentLibrariesTrait;
-use Drupal\ambientimpact_core\ComponentJSSettingsInterface;
-use Drupal\ambientimpact_core\ComponentJSSettingsTrait;
-use Drupal\ambientimpact_core\ComponentHTMLInterface;
-use Drupal\ambientimpact_core\ComponentHTMLTrait;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Base class for implementing Ambient.Impact Component plugins.
  */
 class ComponentBase extends PluginBase implements
-ContainerFactoryPluginInterface, ConfigurableInterface, ComponentInterface,
-ComponentLibrariesInterface, ComponentJSSettingsInterface,
-ComponentHTMLInterface {
-  use ComponentConfigurableTrait;
-  use ComponentLibrariesTrait;
-  use ComponentJSSettingsTrait;
-  use ComponentHTMLTrait;
-
+ContainerFactoryPluginInterface, ConfigurableInterface, ComponentInterface {
   /**
    * The Drupal module handler service.
    *
@@ -90,6 +78,20 @@ ComponentHTMLInterface {
    *   the implementing module's directory.
    */
   protected $path = '';
+
+  /**
+   * Whether this Component has any HTML cached.
+   *
+   * @var null|bool
+   */
+  protected $hasCachedHTML = null;
+
+  /**
+   * This Component's HTML cache ID.
+   *
+   * @var null|string
+   */
+  protected $htmlCacheID = null;
 
   /**
    * Constructs an Ambient.Impact Component object.
@@ -170,5 +172,280 @@ ComponentHTMLInterface {
    */
   public function getPath(): string {
     return $this->path;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfiguration() {
+    return $this->configuration;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @see \Drupal\Core\Block\BlockBase::setConfiguration()
+   *   Copied from this.
+   */
+  public function setConfiguration(array $configuration) {
+    $this->configuration = NestedArray::mergeDeep(
+      $this->baseConfigurationDefaults(),
+      $this->defaultConfiguration(),
+      $configuration
+    );
+  }
+
+  /**
+   * Returns generic default configuration for Component plugins.
+   *
+   * @return array
+   *   An associative array with the default configuration.
+   *
+   * @todo Is there a point to including the plug-in ID in this?
+   */
+  protected function baseConfigurationDefaults() {
+    return [
+      'id' => $this->getPluginId(),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getLibraries(): array {
+    // This component libraries, if any are found.
+    $libraries      = [];
+    // An array of file array references for ease of manipulation: one index for
+    // each 'css' group found, and the 'js' array, if present. At that level
+    // they're structured the same, so this avoids repeating code.
+    $files          = [];
+
+    // Get the YAML parser.
+    $parser         = $this->yamlSerialization;
+    // Get the path to the module implementing this component plugin.
+    $modulePath     = $this->moduleHandler
+      ->getModule($this->pluginDefinition['provider'])->getPath();
+    // This is the path to the component from Drupal's root, including the
+    // implementing module.
+    $componentPath  = $modulePath . '/' . $this->path;
+
+    // This is the full file system path to the file, including the file name
+    // and extension.
+    $filePath =
+      DRUPAL_ROOT . '/' . $componentPath . '/' .
+      $this->pluginDefinition['id'] . '.libraries.yml';
+
+    // Don't proceed if the file doesn't exist.
+    if (!file_exists($filePath)) {
+      return $libraries;
+    }
+
+    // Parse the YAML file.
+    $libraries = $parser::decode(file_get_contents($filePath));
+
+    foreach ($libraries as &$library) {
+      // Save references to each 'css' group array found.
+      if (isset($library['css'])) {
+        foreach ($library['css'] as &$category) {
+          $files[] = &$category;
+        }
+      }
+
+      if (isset($library['js'])) {
+        // Save a reference to the 'js' array, if found.
+        $files[] = &$library['js'];
+
+        // If there no dependencies or the framework isn't found in the
+        // dependencies, insert it.
+        if (
+          !isset($library['dependencies']) ||
+          is_array($library['dependencies']) &&
+          !in_array(
+            'ambientimpact_core/framework',
+            $library['dependencies']
+          )
+        ) {
+          $library['dependencies'][] = 'ambientimpact_core/framework';
+        }
+
+        // If no 'defer' attribute is set, default to true to delay component
+        // JavaScript until most other stuff is done executing. This helps the
+        // page feel a bit faster to load.
+        foreach ($library['js'] as $file => &$fileSettings) {
+          if (!isset($fileSettings['attributes']['defer'])) {
+            $fileSettings['attributes']['defer'] = true;
+          }
+        }
+      }
+    }
+
+    // Prepend the component path to make it relative to the module's directory
+    // as opposed to the component's.
+    foreach ($files as &$category) {
+      foreach (array_keys($category) as $key) {
+        // New key.
+        $category[$this->path . '/' . $key] = $category[$key];
+        // Delete the old key.
+        unset($category[$key]);
+      }
+    }
+
+    return $libraries;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getJSSettings(): array {
+    return [];
+  }
+
+  /**
+   * Get the Component HTML cache settings.
+   *
+   * This can be overridden on a per-Component basis to set custom cache
+   * invalidation.
+   *
+   * This supports 'max-age' and 'tags', but 'contexts' is not yet supported.
+   *
+   * @return array
+   *   The Component HTML cache settings with 'max-age' set to permanent, i.e.
+   *   only rebuilt on a cache rebuild.
+   *
+   * @see https://api.drupal.org/api/drupal/core!core.api.php/group/cache
+   *   Drupal Cache API documentation.
+   *
+   * @todo Add support for cache contexts?
+   *
+   * @see https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Render%21Renderer.php/function/Renderer%3A%3AdoRender
+   *   Cache contexts used in the rendering process.
+   */
+  protected static function getHTMLCacheSettings(): array {
+    return [
+      'max-age' => Cache::PERMANENT,
+    ];
+  }
+
+  /**
+   * Get this Component's HTML cache ID.
+   *
+   * This is only built once and stored for subsequent use.
+   *
+   * @return string
+   *   The value of $this->htmlCacheID.
+   *
+   * @see $this->htmlCacheID
+   *   The HTML cache ID is stored here.
+   */
+  protected function getHTMLCacheID(): string {
+    if ($this->htmlCacheID === null) {
+      $this->htmlCacheID =
+        $this->pluginDefinition['provider'] . ':' .
+        $this->pluginDefinition['id'] . ':' .
+        $this->languageManager->getCurrentLanguage()->getId();
+    }
+
+    return $this->htmlCacheID;
+  }
+
+  /**
+   * Determine if this Component has any cached HTML available.
+   *
+   * @return boolean
+   *   The value of $this->hasCachedHTML.
+   *
+   * @see $this->hasCachedHTML
+   *   Stores whether this Component has cached HTML.
+   */
+  protected function hasCachedHTML(): bool {
+    if ($this->hasCachedHTML === null) {
+      $this->hasCachedHTML = !empty(
+        $this->htmlCacheService->get($this->getHTMLCacheID())->data
+      );
+    }
+
+    return $this->hasCachedHTML;
+  }
+
+  /**
+   * Get the file system path to this Component's HTML file.
+   *
+   * @return string
+   *   The Component's <component name>.html.twig file path.
+   */
+  protected function getHTMLPath() {
+    // Get the path to the module implementing this component plugin.
+    $modulePath = $this->moduleHandler->getModule(
+      $this->pluginDefinition['provider']
+    )->getPath();
+
+    // This is the path to the component from Drupal's root, including the
+    // implementing module.
+    $componentPath  = $modulePath . '/' . $this->path;
+
+    // This is the full file system path to the file, including the file name
+    // and extension.
+    return DRUPAL_ROOT . '/' . $componentPath . '/' .
+      $this->pluginDefinition['id'] . '.html.twig';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function hasHTML(): bool {
+    return file_exists($this->getHTMLPath());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getHTML() {
+    // Don't proceed if a Twig template doesn't exist.
+    if (!$this->hasHTML()) {
+      return false;
+    }
+
+    // If cached HTML is available, grab that without doing any rendering.
+    if ($this->hasCachedHTML()) {
+      $html = $this->htmlCacheService->get($this->getHTMLCacheID())->data;
+
+    // If no cached HTML is found, render and cache the HTML.
+    } else {
+      // Render array containing the file contents as an inline template.
+      $renderArray = [
+        '#type'     => 'inline_template',
+        '#template' => file_get_contents($this->getHTMLPath()),
+      ];
+
+      // Render the inline template.
+      $html = $this->renderer->renderPlain($renderArray);
+
+      $cacheSettings = static::getHTMLCacheSettings();
+
+      // Set the 'max-age' and 'tags' keys if they're not set.
+      if (!isset($cacheSettings['max-age'])) {
+        $cacheSettings['max-age'] = Cache::PERMANENT;
+      }
+      if (!isset($cacheSettings['tags'])) {
+        $cacheSettings['tags'] = [];
+      }
+
+      // Save the rendered template HTML to the cache.
+      $this->htmlCacheService->set(
+        $this->getHTMLCacheID(),
+        $html,
+        $cacheSettings['max-age'],
+        $cacheSettings['tags']
+      );
+    }
+
+    return $html;
   }
 }
--- ambientimpact_core/src/ComponentInterface.php
+++ ambientimpact_core/src/ComponentInterface.php
@@ -12,4 +12,56 @@ interface ComponentInterface {
 	 * @return string
 	 */
 	public function getPath(): string;
+
+  /**
+   * Parse and return any libraries that this component provides.
+   *
+   * Each component can define its own libraries in a
+   * <component name>.libraries.yml file in its component directory. This
+   * functions identically to the module file, with one exception: any file
+   * paths should be relative to the component's directory, as the parent
+   * module's path with be prepended automatically on parsing.
+   *
+   * @return array
+   *
+   * @see https://www.drupal.org/docs/8/creating-custom-modules/adding-stylesheets-css-and-javascript-js-to-a-drupal-8-module
+   */
+  public function getLibraries(): array;
+
+  /**
+   * Return JavaScript settings to pass to the front-end.
+   *
+   * The default implementation just returns an empty array, which is ignored.
+   * Override this to return settings. Don't nest arrays under 'AmbientImpact'
+   * or the component name as that's done automatically.
+   *
+   * @return array
+   *
+   * @see https://www.drupal.org/docs/8/creating-custom-modules/adding-stylesheets-css-and-javascript-js-to-a-drupal-8-module#configurable
+   *
+   * @see \Drupal\ambientimpact_core\ComponentPluginManager\getComponentJSSettings
+   *
+   * @see ambientimpact_core_page_attachments()
+   */
+  public function getJSSettings(): array;
+
+  /**
+   * Determine if this Component has HTML available.
+   *
+   * @return boolean
+   *   True if the <component name>.html.twig file exists, false otherwise.
+   */
+  public function hasHTML(): bool;
+
+  /**
+   * Get any HTML this Component may have available for the front-end.
+   *
+   * @return string|bool
+   *   If the component has a <component name>.html.twig file in its directory,
+   *   it will be rendered and returned, otherwise false is returned.
+   *
+   * @see https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Render%21RendererInterface.php/function/RendererInterface%3A%3Arender
+   *   API documentation for the renderer.
+   */
+  public function getHTML();
 }
