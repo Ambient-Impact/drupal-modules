0c95fd24692b1cfd0963d335a6462ea95ea4a8cd
Ambient.Impact <i@ambientimpact.com>
2019-06-28 09:02:53 -0400

    ComponentInterface.php: converted some tabs to 2 space indent.

--- ambientimpact_core/src/ComponentInterface.php
+++ ambientimpact_core/src/ComponentInterface.php
@@ -6,12 +6,12 @@ namespace Drupal\ambientimpact_core;
  * An interface for all Ambient.Impact Component plugins.
  */
 interface ComponentInterface {
-	/**
-	 * Get the path to this component, relative to the module directory.
-	 *
-	 * @return string
-	 */
-	public function getPath(): string;
+  /**
+   * Get the path to this component, relative to the module directory.
+   *
+   * @return string
+   */
+  public function getPath(): string;
 
   /**
    * Parse and return any libraries that this component provides.
