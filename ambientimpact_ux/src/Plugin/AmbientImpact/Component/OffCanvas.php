881231837ad844d0b6df6cca4809caf82845fc05
Ambient.Impact <i@ambientimpact.com>
2019-06-30 13:52:52 -0400

    Added off-canvas demo.

--- ambientimpact_ux/src/Plugin/AmbientImpact/Component/OffCanvas.php
+++ ambientimpact_ux/src/Plugin/AmbientImpact/Component/OffCanvas.php
@@ -14,4 +14,75 @@ use Drupal\ambientimpact_core\ComponentBase;
  * )
  */
 class OffCanvas extends ComponentBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getDemo(): array {
+    $baseClass = 'offcanvas-demo-panels';
+
+    $lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+    // These are the panels we generate for the demo. The keys are the panel
+    // direction, so they must be valid values that can be passed as the
+    // 'panelLocation' option to the front-end JavaScript.
+    $panels = [
+      'left' => [
+        'actionLabel' => $this->t('Open left'),
+        'panelIntro'  => $this->t('This panel opens from the left side.'),
+      ],
+      'right' => [
+        'actionLabel' => $this->t('Open right'),
+        'panelIntro'  => $this->t('This panel opens from the right side.'),
+      ],
+      'top' => [
+        'actionLabel' => $this->t('Open top'),
+        'panelIntro'  => $this->t('This panel opens from the top.'),
+      ],
+      'bottom' => [
+        'actionLabel' => $this->t('Open bottom'),
+        'panelIntro'  => $this->t('This panel opens from the bottom.'),
+      ],
+    ];
+
+    // This is the container, hidden both visually and in the accessibility
+    // tree.
+    $renderArray = [
+      '#type'       => 'html_tag',
+      '#tag'        => 'div',
+      '#attributes' => ['class' => [$baseClass, 'hidden']],
+      '#attached'   => [
+        'library'     => [
+          'ambientimpact_ux/component.offcanvas.demo',
+        ],
+      ],
+    ];
+
+    // Generate the panel render arrays based on the data in $panels.
+    foreach ($panels as $panelMachineName => $panel) {
+      $renderArray[$panelMachineName] = [
+        '#type'       => 'html_tag',
+        '#tag'        => 'div',
+        '#attributes' => [
+          // 'class'                 => [$baseClass . '__' . $panelMachineName],
+          // The HTML ID is needed to be able to set the 'aria-controls'
+          // attribute on open buttons.
+          'id'                    => $baseClass . '-' . $panelMachineName,
+          'data-panel-direction'  => $panelMachineName,
+          'data-action-label'     => $panel['actionLabel'],
+        ],
+        'intro' => [
+          '#type'     => 'html_tag',
+          '#tag'      => 'p',
+          '#value'    => $panel['panelIntro'],
+        ],
+        'lorem' => [
+          '#type'     => 'html_tag',
+          '#tag'      => 'p',
+          '#value'    => $lorem,
+        ],
+      ];
+    }
+
+    return $renderArray;
+  }
 }
