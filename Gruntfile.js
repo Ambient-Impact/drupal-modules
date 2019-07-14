0381f96f549d1e90dd8ac3c8799857156fcfdcf0
Ambient.Impact <i@ambientimpact.com>
2019-07-14 08:56:21 -0400

    Switched from load-grunt-configs to load-grunt-config for security + updates.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,24 @@ module.exports = function(grunt) {
   'use strict';
 
   // Load our Grunt task configs from external files in the 'grunt' directory.
-  var configs = require('load-grunt-configs')(grunt, {
-    config: {
-      src: 'grunt/*'
+  require('load-grunt-config')(grunt, {
+    init: true,
+    data: {
+      modulePaths:      [
+        'ambientimpact_core',
+        'ambientimpact_block',
+        'ambientimpact_icon',
+        'ambientimpact_media',
+        'ambientimpact_ux',
+        'ambientimpact_web/ambientimpact_web_components',
+      ].join(','),
+
+      stylesheetPaths:  'stylesheets,components',
+      javascriptPaths:  'javascript,components',
+
+      librariesPath:    'assets/vendor'
     }
   });
-
-  // Initialize the Grunt config with the read config plus the package and paths
-  // merged in.
-  grunt.initConfig(Object.assign(configs, {
-    pkg:              grunt.file.readJSON('package.json'),
-
-    modulePaths:      [
-      'ambientimpact_core',
-      'ambientimpact_block',
-      'ambientimpact_icon',
-      'ambientimpact_media',
-      'ambientimpact_ux',
-      'ambientimpact_web/ambientimpact_web_components',
-    ].join(','),
-
-    stylesheetPaths:  'stylesheets,components',
-    javascriptPaths:  'javascript,components',
-
-    librariesPath:    'assets/vendor'
-  }));
-
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-sassdoc');
-  grunt.loadNpmTasks('grunt-postcss');
-  grunt.loadNpmTasks('grunt-modernizr');
-  grunt.loadNpmTasks('grunt-svgstore');
 
   grunt.registerTask('all', [
     'sass',
--- security.md
+++ security.md
@@ -1,5 +1,3 @@
 The following security fixes are found in package.json:
 
-* "tar" is ^2.2.2 to fix [CVE-2018-20834](https://nvd.nist.gov/vuln/detail/CVE-2018-20834)
-* "lodash" is ^4.17.11 to fix [prototype pollution](https://www.npmjs.com/advisories/782)
 * "sassdoc-extras" is ^2.5.1 to fix [Regular Expression Denial of Service](https://github.com/SassDoc/sassdoc-extras/pull/41)
