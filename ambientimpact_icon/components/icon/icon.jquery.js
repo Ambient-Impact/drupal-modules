47882bb4fea6f7966dc8a993e0b208876617c8d2
Ambient.Impact <i@ambientimpact.com>
2019-08-22 17:04:50 -0400

    jQuery icon plug-ins now support multiple target elements.
    jQuery.fn.wrapTextWithIcon() and jQuery.fn.unwrapTextWithIcon()

--- ambientimpact_icon/components/icon/icon.jquery.js
+++ ambientimpact_icon/components/icon/icon.jquery.js
@@ -9,38 +9,61 @@ AmbientImpact.addComponent('icon.jquery', function(aiIconjQuery, $) {
   'use strict';
 
   $.fn.extend({
-    // jQuery plug-in to wrap the text contents of the current element with
-    // a specified icon and settings.
-    wrapTextWithIcon: function(iconCode, settings) {
-      // Grab the existing text and remove it from the DOM.
-      settings.text = this.contents().textNodes().remove().text();
+    /**
+     * Wrap one or more elements' text content in icons.
+     *
+     * @param {String} iconName
+     *   The name of the icon to use.
+     *
+     * @param {Object} settings
+     *   Additional settings to pass to aiIconGet.get()
+     *
+     * @return {jQuery}
+     *   The jQuery collection, for chaining.
+     */
+    wrapTextWithIcon: function(iconName, settings) {
+      return this.each(function() {
+        var $this = $(this);
 
-      // Return the element with the icon inserted.
-      return this.append(aiIconGet.get(iconCode, settings));
+        $this.append(aiIconGet.get(
+          iconName,
+          $.extend(true, {}, settings, {
+            // Grab the existing text after removing it from the DOM.
+            text: $this.contents().textNodes().remove().text()
+          })
+        ));
+      });
     },
-    // jQuery plug-in to unwrap the current element's text contents from an
-    // icon, if one exists.
-    // @see jQuery.fn.wrapTextWithIcon().
+
+    /**
+     * Unwrap one or more elements' text content from an icon, if found.
+     *
+     * @return {jQuery}
+     *   The jQuery collection, for chaining.
+     *
+     * @see jQuery.fn.wrapTextWithIcon()
+     */
     unwrapTextWithIcon: function() {
-      var $icon,
-        containerBaseClass = aiIconGet.settings.containerBaseClass;
+      return this.each(function() {
+        var $this = $(this);
+        var $icon;
+        var containerBaseClass = aiIconGet.settings.containerBaseClass;
 
-      if ($(this).is('.' + containerBaseClass)) {
-        $icon = $(this);
-      } else {
-        $icon = $(this).find('.' + containerBaseClass);
-      }
+        if ($this.is('.' + containerBaseClass)) {
+          $icon = $this;
+        } else {
+          $icon = $this.find('.' + containerBaseClass);
+        }
 
-      if ($icon.length < 1) {
-        return this;
-      }
+        if ($icon.length < 1) {
+          return;
+        }
 
-      $icon.find('.' + containerBaseClass + '__text').contents()
-        .insertAfter($icon);
+        $icon.find('.' + containerBaseClass + '__text').contents()
+          .insertAfter($icon);
 
-      $icon.remove();
-
-      return this;
+        $icon.remove();
+      });
     }
   });
 });
