41d746d81627811c583b25637cb0d5fcae6994c6
Ambient.Impact <i@ambientimpact.com>
2019-08-22 13:38:56 -0400

    Converted icon JS/SCSS to 2 space indent.

--- ambientimpact_icon/components/icon/icon.jquery.js
+++ ambientimpact_icon/components/icon/icon.jquery.js
@@ -3,45 +3,45 @@
 // -----------------------------------------------------------------------------
 
 AmbientImpact.on([
-	'icon.support', 'icon.get', 'jquery',
+  'icon.support', 'icon.get', 'jquery',
 ], function(aiIconSupport, aiIconGet, aijQuery) {
 AmbientImpact.addComponent('icon.jquery', function(aiIconjQuery, $) {
-	'use strict';
+  'use strict';
 
-	$.fn.extend({
-		// jQuery plug-in to wrap the text contents of the current element with
-		// a specified icon and settings.
-		wrapTextWithIcon: function(iconCode, settings) {
-			// Grab the existing text and remove it from the DOM.
-			settings.text = this.contents().textNodes().remove().text();
+  $.fn.extend({
+    // jQuery plug-in to wrap the text contents of the current element with
+    // a specified icon and settings.
+    wrapTextWithIcon: function(iconCode, settings) {
+      // Grab the existing text and remove it from the DOM.
+      settings.text = this.contents().textNodes().remove().text();
 
-			// Return the element with the icon inserted.
-			return this.append(aiIconGet.get(iconCode, settings));
-		},
-		// jQuery plug-in to unwrap the current element's text contents from an
-		// icon, if one exists.
-		// @see jQuery.fn.wrapTextWithIcon().
-		unwrapTextWithIcon: function() {
-			var	$icon,
-				containerBaseClass = aiIconGet.settings.containerBaseClass;
+      // Return the element with the icon inserted.
+      return this.append(aiIconGet.get(iconCode, settings));
+    },
+    // jQuery plug-in to unwrap the current element's text contents from an
+    // icon, if one exists.
+    // @see jQuery.fn.wrapTextWithIcon().
+    unwrapTextWithIcon: function() {
+      var $icon,
+        containerBaseClass = aiIconGet.settings.containerBaseClass;
 
-			if ($(this).is('.' + containerBaseClass)) {
-				$icon = $(this);
-			} else {
-				$icon = $(this).find('.' + containerBaseClass);
-			}
+      if ($(this).is('.' + containerBaseClass)) {
+        $icon = $(this);
+      } else {
+        $icon = $(this).find('.' + containerBaseClass);
+      }
 
-			if ($icon.length < 1) {
-				return this;
-			}
+      if ($icon.length < 1) {
+        return this;
+      }
 
-			$icon.find('.' + containerBaseClass + '__text').contents()
-				.insertAfter($icon);
+      $icon.find('.' + containerBaseClass + '__text').contents()
+        .insertAfter($icon);
 
-			$icon.remove();
+      $icon.remove();
 
-			return this;
-		}
-	});
+      return this;
+    }
+  });
 });
 });
