0039d0026a630c4ae0893825163231b391af5fbd
Ambient.Impact <i@ambientimpact.com>
2019-08-24 17:12:29 -0400

    Added basic menu drop-down component: adds arrow icon; touch support.

---
+++ ambientimpact_ux/components/menu_drop_down/menu_drop_down.js
@@ -0,0 +1,57 @@
+// -----------------------------------------------------------------------------
+//   Ambient.Impact - UX - Menu: drop-down component
+// -----------------------------------------------------------------------------
+
+AmbientImpact.on('icon', function(aiIcon) {
+AmbientImpact.addComponent('menuDropDown', function(aiMenuDropDown, $) {
+  'use strict';
+
+  /**
+   * Expanded menu item link click handler.
+   *
+   * This prevents the default action on the link so that the menu can be
+   * expanded on touch devices.
+   *
+   * @param {jQuery.Event} event
+   */
+  function clickHandler(event) {
+    event.preventDefault();
+  };
+
+  /**
+   * Attach to a provided menu element.
+   *
+   * @param {jQuery|HTMLElement} $menu
+   *   An HTML element or a jQuery collection containing one element. Note that
+   *   if the jQuery collection contains more than one element, only the first
+   *   will be attached to.
+   */
+  this.attach = function($menu) {
+    // Make sure $menu is a jQuery collection by having jQuery wrap it. If it's
+    // already a jQuery collection, jQuery will return it as-is.
+    $menu = $($menu).first();
+
+    $menu.find('.menu-item--expanded > a')
+      .wrapTextWithIcon('arrow-down', {bundle: 'core'})
+      .on('click.aiMenuDropDown', clickHandler);
+  };
+
+  /**
+   * Detach from a provided menu element.
+   *
+   * @param {jQuery|HTMLElement} $menu
+   *   An HTML element or a jQuery collection containing one element. Note that
+   *   if the jQuery collection contains more than one element, only the first
+   *   will be detached from.
+   */
+  this.detach = function($menu) {
+    // Make sure $menu is a jQuery collection by having jQuery wrap it. If it's
+    // already a jQuery collection, jQuery will return it as-is.
+    $menu = $($menu).first();
+
+    $menu.find('.menu-item--expanded > a')
+      .unwrapTextWithIcon()
+      .off('click.aiMenuDropDown', clickHandler);
+  };
+});
+});
---
+++ ambientimpact_ux/src/Plugin/AmbientImpact/Component/MenuDropDown.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_ux\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Menu: drop-down component.
+ *
+ * @Component(
+ *   id = "menu_drop_down",
+ *   title = @Translation("Menu: drop-down"),
+ *   description = @Translation("Improves the accessibility and UX of drop-down menus.")
+ * )
+ */
+class MenuDropDown extends ComponentBase {
+}
