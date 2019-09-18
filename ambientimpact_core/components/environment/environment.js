827c345a32dcaaea6af6a137a72bab83ea4d7c89
Ambient.Impact <i@ambientimpact.com>
2019-09-17 21:22:12 -0400

    Added JSDoc blocks for aiEnvironment.cssSupports().

--- ambientimpact_core/components/environment/environment.js
+++ ambientimpact_core/components/environment/environment.js
@@ -8,16 +8,42 @@
 AmbientImpact.addComponent('environment', function(aiEnvironment, $) {
   'use strict';
 
-  // CSS.supports() wrapper. Removes need to check for presence of
-  // CSS.supports().
   if (AmbientImpact.objectPathExists('CSS.supports')) {
+    /**
+     * CSS.supports() wrapper when it's available.
+     *
+     * @param {String} property
+     *   The CSS property name to test for.
+     *
+     * @param {String} value
+     *   The CSS property value to test for, for the 'property' parameter.
+     *
+     * @return {Boolean}
+     *   The return value of CSS.supports() based on the passed property and
+     *   value.
+     */
     this.cssSupports = function(property, value) {
       return CSS.supports(property, value);
     }
+
   } else {
+    /**
+     * CSS.supports() dummy method for when it isn't supported.
+     *
+     * This is provided to avoid errors in browsers that don't have
+     * CSS.supports() available, so that this method can safely be called
+     * without having to check.
+     *
+     * @param {String} property
+     *   The CSS property name to test for.
+     *
+     * @param {String} value
+     *   The CSS property value to test for, for the 'property' parameter.
+     *
+     * @return {Boolean}
+     *   Always returns false.
+     */
     this.cssSupports = function(property, value) {
-      // If not supported, always return false. We could use a polyfill
-      // for this, but those browsers are dwindling in number.
       return false;
     }
   }
