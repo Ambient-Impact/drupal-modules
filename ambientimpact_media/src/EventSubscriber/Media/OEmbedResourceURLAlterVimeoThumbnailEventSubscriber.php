fb644b2bae567c433d98cc8467a733736fe1013d
Ambient.Impact <i@ambientimpact.com>
2019-12-26 10:34:32 -0500

    ambientimpact_media: converted hook_oembed_resource_url_alter to event.

---
+++ ambientimpact_media/src/EventSubscriber/Media/OEmbedResourceURLAlterVimeoThumbnailEventSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\ambientimpact_media\EventSubscriber\Media;
+
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\hook_event_dispatcher\Event\Media\OEmbedResourceUrlAlterEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Vimeo thumbnail oEmbed hook_oembed_resource_url_alter() event subscriber.
+ */
+class OEmbedResourceURLAlterVimeoThumbnailEventSubscriber implements
+EventSubscriberInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      HookEventDispatcherInterface::MEDIA_OEMBED_RESOURCE_DATA_ALTER =>
+        'oEmbedResourceURLAlter',
+    ];
+  }
+
+  /**
+   * Get a high resolution thumbnail for Vimeo oEmbed resources.
+   *
+   * This informs Vimeo to return the thumbnail at the maximum size they
+   * support, which is currently 1280 pixels wide.
+   *
+   * @param \Drupal\hook_event_dispatcher\Event\Media\OEmbedResourceUrlAlterEvent $event
+   *   The event object.
+   */
+  public function oEmbedResourceURLAlter(OEmbedResourceUrlAlterEvent $event) {
+    /** @var array */
+    $parsedURL = &$event->getParsedURL();
+
+    if (
+      \strpos($parsedURL['path'], 'https://vimeo.com/') === 0 &&
+      !isset($parsedURL['query']['width'])
+    ) {
+      $parsedURL['query']['width'] = '1280';
+    }
+  }
+}
