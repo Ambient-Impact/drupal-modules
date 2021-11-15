724ac0a3ba0a992d28ed7c8d7dfc143e8b82a426
Matei Stanca <i@ambientimpact.com>
2021-11-15 15:55:49 -0500

    Moved ambientimpact_core/stylesheets to new sass component.

---
+++ ambientimpact_core/src/Plugin/AmbientImpact/Component/Sass.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\ambientimpact_core\Plugin\AmbientImpact\Component;
+
+use Drupal\ambientimpact_core\ComponentBase;
+
+/**
+ * Sass component.
+ *
+ * @Component(
+ *   id           = "sass",
+ *   title        = @Translation("Sass"),
+ *   description  = @Translation("Contains various Sass partials.")
+ * )
+ */
+class Sass extends ComponentBase {
+}
